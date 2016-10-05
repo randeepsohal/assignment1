922f48dbdb3aae8b15a6724ce254bcedb37328e9
randeepsohal <randeepsohal1918@yahoo.com>
2016-10-05 12:30:11 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
