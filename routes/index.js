118f213e66825e3728c1c845adac2f2c6086da68
randeepsohal <randeepsohal1918@yahoo.com>
2016-10-12 22:57:06 -0400

    another commit

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,9 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
 });
 
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+router.get('/projects', function (req,res,next) {
+  res.render('projects', { title: 'Project'});
+
 });
 
 router.get('/contact', function(req, res, next) {
